9be54fd8a725b551cb856a3ad7e27befc9e238e9
Melvin <apple@apples-mbp.home>
2024-09-22 21:03:37 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,53 @@
+public class Polynomial {
+    double[] coefficients;
+        
+    Polynomial(){
+        this.coefficients = new double[] {0};
+        }
+        
+    Polynomial(double[] givenCoefficients){
+        this.coefficients = new double[givenCoefficients.length];
+        for(int i=0; i<coefficients.length; i++){
+            this.coefficients[i]= givenCoefficients[i];
+            }
+        }
+    Polynomial add(Polynomial givenPoly){
+        if(this.coefficients.length ==0){
+            return givenPoly;
+            }
+        else{
+            if(givenPoly.coefficients.length > this.coefficients.length){
+                double[] placeHolder = givenPoly.coefficients;
+                givenPoly.coefficients = this.coefficients;
+                this.coefficients = placeHolder;
+            }
+            for(int i=0; i<givenPoly.coefficients.length; i++){
+                this.coefficients[i] = this.coefficients[i] + givenPoly.coefficients[i];
+                }
+            return this;
+            }
+        }
+    
+    double evaluate(double x){
+        double sum = 0.0;
+        for(int i=0; i< coefficients.length;i++){
+            sum += this.coefficients[i]*(Math.pow(x, i));
+            }
+        return sum;
+        }
+    
+    boolean hasRoot(double x){
+        double sum = 0.0;
+        for(int i=0; i< coefficients.length;i++){
+            sum = sum + (this.coefficients[i]*(Math.pow(x, i)));
+            }
+        if(sum == 0.0){
+            return true;
+        }
+        else{
+             return false;        
+        }
+        
+    }
+}
+    
